265846764eaaf493ac332ce460ab7796a8b10db5
Metabeast <84871625+Meetabeast@users.noreply.github.com>
2022-12-29 20:30:53 +0100

    Add files via upload

---
+++ types.js
@@ -0,0 +1,22 @@
+let ActivityTypes = {
+    0: "Playing",
+    1: "Streaming",
+    2: "Listening",
+    3: "Watching",
+    4: "Custom",
+    5: "Competing"
+}
+
+let Themes = {
+    "dark": "Dark",
+    "white": "White",
+    "transparent": "Transparent",
+    "green": "Green",
+    "red": "Red",
+    "blue": "Blue"
+}
+
+module.exports = {
+    ActivityTypes,
+    Themes
+}
